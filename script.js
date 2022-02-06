583190adc3b3bec6568fff16818bb66c19db9f4a
ericac21 <99135583+ericac21@users.noreply.github.com>
2022-02-06 11:53:26 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+//To make tabs into a bar
+const tags = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.tabTarget)
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('active')
+    })
+    tab.forEach(tab => {
+      tab.classList.remove('active')
+    })
+    tab.classList.add('active')
+    target.classList.add('active')
+  })
+})
+
+
+
